e31819e772d6e101cbc1a17511fc797beab1bc09
AMV Vault <recordingsabantu@gmail.com>
2026-03-02 18:46:04 +0200

    Update supabase.ts

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -3,4 +3,5 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
+// This is the line that exports it so the login page can see it
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
